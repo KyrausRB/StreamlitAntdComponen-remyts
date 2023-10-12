a0e607577120335b8741f15fa981a5f93e0301bc
KyrausRB <137050927+KyrausRB@users.noreply.github.com>
2023-10-12 13:46:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme() -> str:
 
 setuptools.setup(
     name="streamlit-antd-components",
-    version="0.2.3",
+    version="0.2.2",
     author="jihaoran",
     description="streamlit components of antd design",
     long_description=readme(),
